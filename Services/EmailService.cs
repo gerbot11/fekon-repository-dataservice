50e31c5cbb6fb6053e79d702bdac49e86d182ba5
Gery Soumokil <gerbot08@hotmail.com>
2022-04-03 19:05:57 +0700

    tambah email service registrasi akun

---
+++ Services/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fekon_repository_dataservice.Services
+{
+    public class EmailService : IEmailSender
+    {
+        public EmailService()
+        {
+            
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            string fromMail = "dj.gerbot.music@gmail.com";
+            string fromPassword = "pvgoqyvmbeworyjt";
+            MailMessage message = new();
+            message.From = new MailAddress(fromMail);
+            message.Subject = subject;
+            message.To.Add(new MailAddress(email));
+            message.Body = "<html><body> " + htmlMessage + " </body></html>";
+            message.IsBodyHtml = true;
+
+            SmtpClient smtpClient = new("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromMail, fromPassword),
+                EnableSsl = true,
+            };
+
+            await smtpClient.SendMailAsync(message);
+        }
+    }
+}
